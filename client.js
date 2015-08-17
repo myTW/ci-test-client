50ec2bb0ce4e0278bdbb094e54d67a11a9791a69
Zhang Xinyu <iamzhangxinyu@gmail.com>
2015-08-17 21:20:43 +0800

    refactor

--- client.js
+++ client.js
@@ -1,7 +1,5 @@
-#!/usr/bin/env node
-
+var fs = require('fs');
 var dgram = require('dgram');
-var message = new Buffer("0");
 var server = dgram.createSocket("udp4");
 var client = dgram.createSocket("udp4");
 
@@ -10,12 +8,14 @@ var selfPort = 41235;
 var remotePort = 41234;
 
 server.on("message", function (msg, err) {
-
     console.log('reveived: ' + msg);
+
+    var status = fs.readFileSync("/sys/class/leds/green\:ph24\:led1/brightness");
+    var message = new Buffer(status);
 
     client.send(message, 0, message.length, remotePort, REMOTE, function (err) {
         client.close();
     });
 });
 
-server.bind(selfPort, 'localhost');
+server.bind(selfPort, '0.0.0.0');
